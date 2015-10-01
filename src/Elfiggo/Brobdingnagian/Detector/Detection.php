5715f174cc395f2acd2093c1363c2502f198b67a
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2015-10-01 05:46:04 +0100

    Added Interface docbloc

--- src/Elfiggo/Brobdingnagian/Detector/Detection.php
+++ src/Elfiggo/Brobdingnagian/Detector/Detection.php
@@ -2,12 +2,14 @@
 
 namespace Elfiggo\Brobdingnagian\Detector;
 
-
 use Elfiggo\Brobdingnagian\Param\Params;
 use Elfiggo\Brobdingnagian\Report\Reporter;
 use ReflectionClass;
 
-
+/**
+ * Interface Detection
+ * @package Elfiggo\Brobdingnagian\Detector
+ */
 interface Detection
 {
     public function check(ReflectionClass $sus, Params $param, Reporter $reporter);
