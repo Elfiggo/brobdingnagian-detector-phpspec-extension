bd82be3a4f0e4dd7578dc1a8700a525dab4d7a0e
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2015-10-01 05:50:19 +0100

    Added Class docbloc and reduced line length

--- src/Elfiggo/Brobdingnagian/Detector/NumberOfMethods.php
+++ src/Elfiggo/Brobdingnagian/Detector/NumberOfMethods.php
@@ -6,18 +6,27 @@ use Elfiggo\Brobdingnagian\Param\Params;
 use Elfiggo\Brobdingnagian\Report\Reporter;
 use ReflectionClass;
 
+/**
+ * Class NumberOfMethods
+ * @package Elfiggo\Brobdingnagian\Detector
+ */
 class NumberOfMethods implements Detection
 {
 
     /**
      * @param ReflectionClass $sus
-     * @param Params $params
-     * @param Reporter $reporter
+     * @param Params          $params
+     * @param Reporter        $reporter
      */
     public function check(ReflectionClass $sus, Params $params, Reporter $reporter)
     {
         if (count($sus->getMethods()) > $params->getNumberOfMethods()) {
-            $reporter->act($sus, self::class, "{$sus->getName()} has too many methods (" . count($sus->getMethods()) .')', 'Too many methods');
+            $reporter->act(
+                $sus,
+                self::class,
+                "{$sus->getName()} has too many methods (" . count($sus->getMethods()) .')',
+                'Too many methods'
+            );
         }
     }
 }
