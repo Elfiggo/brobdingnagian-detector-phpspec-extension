ee40a96a7dc5f0a97eb5e5468c374d7d7b43afe6
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2015-10-01 05:43:04 +0100

    Added Class docbloc and reduced line length

--- src/Elfiggo/Brobdingnagian/Detector/ClassSize.php
+++ src/Elfiggo/Brobdingnagian/Detector/ClassSize.php
@@ -6,19 +6,23 @@ use Elfiggo\Brobdingnagian\Param\Params;
 use Elfiggo\Brobdingnagian\Report\Reporter;
 use ReflectionClass;
 
-
-
+/**
+ * Class ClassSize
+ * @package Elfiggo\Brobdingnagian\Detector
+ */
 class ClassSize implements Detection
 {
     /**
      * @param ReflectionClass $sus
-     * @param Params $params
-     * @param Reporter $reporter
+     * @param Params          $params
+     * @param Reporter        $reporter
      */
     public function check(ReflectionClass $sus, Params $params, Reporter $reporter)
     {
         if ($sus->getEndLine() > $params->getClassSize()) {
-            $reporter->act($sus, self::class, "{$sus->getName()} class size is too large ({$sus->getEndLine()})", 'Class size');
+            $reporter->act(
+                $sus, self::class, "{$sus->getName()} class size is too large ({$sus->getEndLine()})", 'Class size'
+            );
         }
     }
 }
